859a674f08d7f4d0354156c37f7c46eda647ce1d
Nikola <nikola.dj.87@gmail.com>
2022-02-01 23:55:07 +0100

    added basic structure and glitch effect to the name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from './pages/about/About'
+import Home from './pages/home/Home'
+import Projects from './pages/projects/Projects'
+import SideMenu from './components/sidebarMenu/SidebarMenu'
+import video from './components/video/2.mp4'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <video id="bgVid" autoPlay loop muted >
+          <source src={video} type="video/mp4" />
+        </video>
+        <div className="frame"
+         style={{ height: "100vh", width: '100%', backgroundImage: 'url("/images/CYBFrameBackground.png")', backgroundPosition: 'bottom left', backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
+         >
+        </div>
+        <SideMenu/>
+         <div className="pages">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
     </div>
   );
 }
---
+++ src/components/sidebarMenu/SidebarMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './sidebarMenu.scss'
+
+function SidebarMenu() {
+  return <div className="sidebar">
+      <div className="sidebar__content">
+       <div className="burger">
+         <div className="line-1"></div>
+         <div className="line-2"></div>
+         <div className="line-3"></div>
+       </div>
+      </div>
+    </div>
+   ;
+}
+
+export default SidebarMenu;

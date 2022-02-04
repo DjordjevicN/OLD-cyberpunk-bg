151134febc7a54094fde11487776b32b05a46ad9
Nikola <nikola.dj.87@gmail.com>
2022-02-04 15:43:56 +0100

    added menu and redux

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-
-import './App.scss';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import About from './pages/about/About'
-import Home from './pages/home/Home'
-import Projects from './pages/projects/Projects'
-import SideMenu from './components/sidebarMenu/SidebarMenu'
-import video from './components/video/2.mp4'
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Projects from "./pages/projects/Projects";
+import SideMenu from "./components/sidebarMenu/SidebarMenu";
+import video from "./components/video/2.mp4";
 
 function App() {
   return (
     <div className="App">
-         <video id="bgVid" autoPlay loop muted >
-          <source src={video} type="video/mp4" />
-        </video>
-        <div className="frame"
-         style={{ height: "100vh", width: '100%', backgroundImage: 'url("/images/CYBFrameBackground.png")', backgroundPosition: 'bottom left', backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
-         >
-        </div>
-        <SideMenu/>
-         <div className="pages">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
+      <video id="bgVid" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div
+        className="frame"
+        style={{
+          height: "100vh",
+          width: "100%",
+          backgroundImage: 'url("/images/CYBFrameBackground.png")',
+          backgroundPosition: "bottom left",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <SideMenu />
+      <div className="pages">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/components/sidebarMenu/SidebarMenu.js
+++ src/components/sidebarMenu/SidebarMenu.js
@@ -1,14 +1,65 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './sidebarMenu.scss'
+import { motion } from 'framer-motion'
 
 function SidebarMenu() {
+const [isOpen,setIsOpen]=useState(true)
+
+const burger = {
+  open: { rotate: -90, },
+  closed: {  rotate: 0,   transition:{
+    delay:.5,
+ 
+  } },
+ 
+}
+const line1 = {
+  open: { y: -490,  transition:{
+    delay:.3,
+    duration:.3
+  } },
+  closed: { y: 0,
+     transition:{
+    delay:.3,
+    duration:.3
+  } },
+}
+const menu = {
+  open: {opacity:1, x:0,  transition:{
+    delay:.4,
+    duration:.3
+  } },
+  closed: { opacity: 0,x:30,
+     transition:{
+    delay:.25,
+    duration:.2
+  } },
+}
+
+
   return <div className="sidebar">
       <div className="sidebar__content">
-       <div className="burger">
-         <div className="line-1"></div>
-         <div className="line-2"></div>
-         <div className="line-3"></div>
-       </div>
+       <motion.div className="burger"
+       animate={isOpen ? "open" : "closed"}
+       variants={burger}
+    
+       onClick={() => setIsOpen(isOpen => !isOpen)} >
+         <motion.div className="line-1" 
+         animate={isOpen ? "open" : "closed"}
+         variants={line1}
+       
+         ></motion.div>
+         <div className="line-2" style={{opacity:isOpen?0:1}}></div>
+         
+         <div className="line-3" style={{width:isOpen?'50px':'40px'}}></div>
+       </motion.div>
+       <motion.div className="menuLinks"   
+        animate={isOpen ? "open" : "closed"}
+         variants={menu}>
+           <a href='/'  className="linkItem">HOME</a>
+           <a href='/projects' className="linkItem">PROJECTS</a>
+           <a href='/about' className="linkItem">ABOUT ME</a>
+         </motion.div>
       </div>
     </div>
    ;

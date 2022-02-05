9fec4dd73be00fef93f65a243d45952961e53ab5
Nikola <nikola.dj.87@gmail.com>
2022-02-05 10:11:15 +0100

    fixing edges

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -8,22 +8,25 @@ function About() {
         <div className="inner-content">
           <p>
             Hi, I'm Nikola Djordjevic, a front-end developer based in Belgrade,
-            Serbia. My main stack is JavaScript, React, Redux, SCSS. For the
-            past six months, I have been working on developing a dispatch
-            dashboard for a transportation company. I love developing
-            user-experience based projects. I have a keen eye on animation and
-            website design, too.
+            Serbia.
+            <br /> For the past six months, I have been working on developing a
+            dispatch dashboard, booking website and business website for a
+            transportation company. I love developing user-experience based
+            projects. I have a keen eye on animation and website design, too.
           </p>
           <br />
           <p>
             Wanna get to know me more? Feel free to get in touch with me via
-            e-mail, or social media linked below.
+            <a href="mailto:nikola.dj.87@gmail.com"> e-mail</a> , or social
+            media linked below.
           </p>
 
           <div className="links">
-            <a href="/">LinkedIn</a>
-            <a href="/">Twitter</a>
-            <a href="/">Github</a>
+            <a href="https://www.linkedin.com/in/nikola-djordjevic-503066193/">
+              LinkedIn
+            </a>
+            <a href="https://twitter.com/DjordjevicNix">Twitter</a>
+            <a href="https://github.com/DjordjevicN">Github</a>
           </div>
         </div>
         <div className="edge"></div>
